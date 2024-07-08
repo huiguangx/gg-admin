7f60197ab84311b326cc773ccb0ff1b5958e74d0
guang <531311582@qq.com>
2024-07-08 22:07:58 +0800

    feat: 配置环境变量文件+svg图标配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import IconTest from '@/assets/vue.svg'
 
   defineProps<{ msg: string }>()
 
@@ -11,6 +12,7 @@
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <IconTest />
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,6 +32,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    svgLoader(),
   ],
   resolve: {
     alias: {
